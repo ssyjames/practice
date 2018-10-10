bce358e1c4b52e2b1f5d90f7b26baa770da854c2
sue-tania <sue-tania.james@govt.lc>
2018-10-10 14:22:58 -0400

    first commit

---
+++ git-repository/Benchmark.java
@@ -0,0 +1,29 @@
+package com.java24hours;
+
+public class Benchmark {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		long startTime = System.currentTimeMillis();
+		long endTime = startTime + 60000;
+		long index = 0;
+		
+		while (true) {
+			double x = Math.sqrt(index);
+			long now = System.currentTimeMillis();
+			
+			if (now > endTime) {
+				break;
+				
+			}
+			
+			index++;
+			
+		}
+		
+		System.out.println(index + " loops in one minute.");
+
+	}
+
+}
